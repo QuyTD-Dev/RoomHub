7e1c6d6163395a00bb5fddb02a2c4f7121139a33
phanhoaian1203 <phanhoaian10b3@gmail.com>
2026-03-08 17:42:02 +0700

    add Chat UI

---
+++ Web/Controllers/ChatController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    public class ChatController : Controller
+    {
+        // Giao diện Chat dành cho Người thuê nhà
+        public IActionResult Tenant()
+        {
+            return View();
+        }
+
+        // Giao diện Chat dành cho Chủ nhà
+        public IActionResult Owner()
+        {
+            return View();
+        }
+    }
+}
