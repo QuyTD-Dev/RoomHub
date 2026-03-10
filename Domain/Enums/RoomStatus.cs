21b23c872ce0160cd4de09faba2e2b6a7855beba
Trần Đình Quý <160567708+QuyTD-Dev@users.noreply.github.com>
2026-03-10 23:15:46 +0700

    feat(domain): add Room entity, RoomStatus enum and update ApplicationUser

--- Domain/Enums/RoomStatus.cs
+++ Domain/Enums/RoomStatus.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +11,8 @@ namespace Domain.Enums
         Available,
         Deposited,
         Occupied,
-        UnderMaintenance
+        UnderMaintenance,
+        Active,
+        Hidden
     }
 }
