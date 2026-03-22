2f852282510033d25021cfbb3db721d952780664
NguyenHongAn12 <anltne0201@gmail.com>
2026-03-22 18:24:12 +0700

    feature: fix search-room

--- Infrastructure/Persistence/Migrations/20260322110114_AddReviewBlockedUntil.Designer.cs
+++ Infrastructure/Persistence/Migrations/20260322110114_AddReviewBlockedUntil.Designer.cs
@@ -4,6 +4,7 @@ using Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260322110114_AddReviewBlockedUntil")]
+    partial class AddReviewBlockedUntil
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
